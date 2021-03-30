8003e22eddd07231a9607c8c2221711c56a91379
Kulbhushan Singh <34381774+Kulbhushan2703@users.noreply.github.com>
2021-03-31 03:55:02 +0530

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Title({name, Title}) {
+    name = name + ' PRODUCTS';
     return (
         <div className='row'>
             <div className='col-10 mx-auto my-2 text-center text-title'>
